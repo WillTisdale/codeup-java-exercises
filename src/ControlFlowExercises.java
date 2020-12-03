ce36ec46c326a0555b91bebe35a2b4bd30c68d40
William Tisdale <will.tisdale.31@gmail.com>
2020-12-03 14:17:08 -0600

    completed exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -16,13 +16,32 @@ public class ControlFlowExercises {
 //            System.out.println(n);
 //        } while(n > 100);
 
-        for (int num = 100; num >= -10; num -=5){
-            System.out.println(num);
-        }
+//        for (int num = 100; num >= -10; num -=5){
+//            System.out.println(num);
+//        }
+//
+//        for (long d = 2; d < 1000000; d = d * d){
+//            System.out.println(d);
+//        }
 
-        for (long d = 2; d < 1000000; d = d * d){
-            System.out.println(d);
-        }
+//        String fizz = "Fizz";
+//        String buzz = "Buzz";
+//        String fizzBuzz = "FizzBuzz";
+//        for(int i = 1; i <= 100; i++){
+//            if(i % 15 == 0){
+//                System.out.println(fizzBuzz);
+//                continue;
+//            }
+//            if(i % 5 == 0){
+//                System.out.println(buzz);
+//                continue;
+//            }
+//            if(i % 3 == 0){
+//                System.out.println(fizz);
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
 
     }
 
