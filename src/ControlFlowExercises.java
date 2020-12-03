a06d442d388a844c80e4fa7eeb33ed2a62849a87
William Tisdale <will.tisdale.31@gmail.com>
2020-12-03 16:02:10 -0600

    completed all exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
@@ -46,36 +47,104 @@ public class ControlFlowExercises {
 //        }
 
         Scanner sc = new Scanner(System.in);
-        System.out.printf("What number would you like to go up to? ");
-        int n = sc.nextInt();
-        System.out.println();
-        System.out.println("Here is your table!");
-        System.out.println();
-        System.out.println("number  |  squared  |  cubed");
-        System.out.println("------  |  -------  |  -----");
-        for (int i = 1; i <= n; i++){
-            int number = i;
-            int squared = i*i;
-            int cubed = i*i*i;
-            if (number <= 9){
-                System.out.printf("%d       |", number);
-            }
-            if (number > 9 && number < 100){
-                System.out.printf("%d      |", number);
-            }
-            if (squared <= 9){
-                System.out.printf("  %d        |", squared);
-            }
-            if (squared > 9 && number < 100){
-                System.out.printf("  %d       |", squared);
-            }
-            if (cubed <= 9) {
-                System.out.printf("  %d    %n", cubed);
-            }
-            if (cubed > 9 && number < 100){
-                System.out.printf("  %d   %n", cubed);
-            }
-        }
+
+//        System.out.printf("What number would you like to go up to? ");
+//        int n = sc.nextInt();
+//        System.out.println();
+//        System.out.println("Here is your table!");
+//        System.out.println();
+//        System.out.println("number  |  squared  |  cubed");
+//        System.out.println("------  |  -------  |  -----");
+//        for (int i = 1; i <= n; i++){
+//            int number = i;
+//            int squared = i*i;
+//            int cubed = i*i*i;
+//            if (number <= 9){
+//                System.out.printf("%d       |", number);
+//            }
+//            if (number > 9 && number < 100){
+//                System.out.printf("%d      |", number);
+//            }
+//            if (squared <= 9){
+//                System.out.printf("  %d        |", squared);
+//            }
+//            if (squared > 9 && number < 100){
+//                System.out.printf("  %d       |", squared);
+//            }
+//            if (cubed <= 9) {
+//                System.out.printf("  %d    %n", cubed);
+//            }
+//            if (cubed > 9 && number < 100){
+//                System.out.printf("  %d   %n", cubed);
+//            }
+//        }
+
+        System.out.printf("Enter a numerical grade between 0 and 100: ");
+        int grade = sc.nextInt();
+        String more = "Yes";
+        int count = 0;
+            do {
+                ++count;
+                if (grade > 100 || grade < 0){
+                    --count;
+                    System.out.println("You must enter a number between 0 and 100.");
+                }
+                if (grade <= 100 && grade >= 88) {
+                    if (grade >= 99) {
+                        System.out.printf("%d.) %d is an A+%n", count, grade);
+                    } else if (grade <= 89) {
+                        System.out.printf("%d.) %d is an A-%n", count, grade);
+                    } else {
+                        System.out.printf("%d.) %d is an A%n", count, grade);
+                    }
+                }
+                if (grade <= 87 && grade >= 80) {
+                    if (grade >= 86) {
+                        System.out.printf("%d.) %d is a B+%n", count, grade);
+                    } else if (grade <= 81) {
+                        System.out.printf("%d.) %d is a B-%n", count, grade);
+                    } else {
+                        System.out.printf("%d.) %d is a B%n", count, grade);
+                    }
+                }
+                if (grade <= 79 && grade >= 67) {
+                    if (grade >= 78) {
+                        System.out.printf("%d.) %d is a C+%n", count, grade);
+                    } else if (grade <= 68) {
+                        System.out.printf("%d.) %d is a C-%n", count, grade);
+                    } else {
+                        System.out.printf("%d.) %d is a C%n", count, grade);
+                    }
+                }
+                if (grade <= 66 && grade >= 60) {
+                    if (grade >= 65) {
+                        System.out.printf("%d.) %d is a D+%n", count, grade);
+                    } else if (grade <= 61) {
+                        System.out.printf("%d.) %d is a D-%n", count, grade);
+                    } else {
+                        System.out.printf("%d.) %d is a D%n", count, grade);
+                    }
+                }
+                if (grade <= 59 && grade >= 0) {
+                    System.out.printf("%d.) %d is an F%n", count, grade);
+                }
+                try
+                {
+                    System.out.printf("Enter another grade: ");
+                    grade = sc.nextInt();
+                }
+                catch (InputMismatchException exception)
+                {
+                    System.out.println("Thank you!");
+                    more = "no";
+                }
+            }while(more.equalsIgnoreCase("yes"));
+
+
+
+
+
+
     }
 
 }
