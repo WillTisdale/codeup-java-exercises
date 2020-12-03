be088d2e4263da94ae863b5acc064aab11523015
William Tisdale <will.tisdale.31@gmail.com>
2020-12-03 11:16:26 -0600

    updated ConsoleExercises post walk-through

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,52 +6,50 @@ public class ConsoleExercises {
 
     public static void main(String[] args) {
 
-//        double pi = 3.14159;
-//        System.out.printf("The value of pi is approximately %.2f.%n", pi);
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f.%n", pi);
 
         Scanner userInput = new Scanner(System.in);
-        userInput.useDelimiter("\n");
+//        userInput.useDelimiter("\n");
 
-//        System.out.printf("Enter an integer.%n");
-//        int userInt = userInput.nextInt();
-//        System.out.println(userInt);
+        System.out.printf("Enter an integer.%n");
+        int userInt = userInput.nextInt();
+        System.out.printf("Your integer is: %d.%n", userInt);
 
-//        System.out.printf("Enter three words.%n");
-//        String firstWord = userInput.next();
-//        String secondWord = userInput.next();
-//        String thirdWord = userInput.next();
-//        System.out.println(firstWord);
-//        System.out.println(secondWord);
-//        System.out.println(thirdWord);
+        System.out.printf("Enter three words.%n");
+        String firstWord = userInput.next();
+        String secondWord = userInput.next();
+        String thirdWord = userInput.next();
+        System.out.printf("Your three words are: %s, %s, and %s.%n", firstWord, secondWord, thirdWord);
 
-//        System.out.printf("Enter a sentence.%n");
-//        String userString = userInput.nextLine();
-//        System.out.println(userString);
+        System.out.printf("Enter a sentence.%n");
+        String userString = userInput.nextLine();
+        System.out.printf("Your sentence is: '%s'.%n", userString);
+
+        System.out.printf("Enter values of length, width, and height of a Codeup classroom.%n");
+        float userLength = Float.parseFloat(userInput.next());
+        float userWidth = Float.parseFloat(userInput.next());
+        float userHeight = Float.parseFloat(userInput.next());
+        float area = userWidth * userLength;
+        float perimeter = (userWidth*2) + (userLength*2);
+        float volume = userWidth * userLength * userHeight;
+        System.out.printf("Length: %.2f.%n", userLength);
+        System.out.printf("Width: %.2f.%n", userWidth);
+        System.out.printf("Height: %.2f.%n", userHeight);
+        System.out.printf("Area: %.2f%n", area);
+        System.out.printf("Perimeter: %.2f%n", perimeter);
+        System.out.printf("Volume: %.2f%n", volume);
 
 //        System.out.printf("Enter values of length, width, and height of a Codeup classroom.%n");
-//        String userDemensions = userInput.nextLine();
-//        System.out.println(userDemensions);
-//        String numbers [] = userDemensions.split(" ");
-//        float userLength = Float.parseFloat(numbers[1]);
-//        float userWidth = Float.parseFloat(numbers[0]);
-//        float userHeight = Float.parseFloat(numbers[2]);
-//        float area = userWidth * userLength;
+//        float userLength = userInput.nextFloat();
+//        float userWidth = userInput.nextFloat();
+//        float userHeight = userInput.nextFloat();
+//        float area = userLength * userWidth;
 //        float perimeter = (userWidth*2) + (userLength*2);
 //        float volume = userWidth * userLength * userHeight;
 //        System.out.printf("The area of the classroom is: %.2f%n", area);
 //        System.out.printf("The perimeter of the classroom is: %.2f%n", perimeter);
 //        System.out.printf("The volume of the classroom is: %.2f%n", volume);
-
-        System.out.printf("Enter values of length, width, and height of a Codeup classroom.%n");
-        float userLength = userInput.nextFloat();
-        float userWidth = userInput.nextFloat();
-        float userHeight = userInput.nextFloat();
-        float area = userLength * userWidth;
-        float perimeter = (userWidth*2) + (userLength*2);
-        float volume = userWidth * userLength * userHeight;
-        System.out.printf("The area of the classroom is: %.2f%n", area);
-        System.out.printf("The perimeter of the classroom is: %.2f%n", perimeter);
-        System.out.printf("The volume of the classroom is: %.2f%n", volume);
 
     }
 
