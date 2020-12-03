1625f17c1c9274312a7a624c9c60b4fc1aa13a99
William Tisdale <will.tisdale.31@gmail.com>
2020-12-03 14:40:24 -0600

    completed exercise 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args){
@@ -43,6 +45,37 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("What number would you like to go up to? ");
+        int n = sc.nextInt();
+        System.out.println();
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("number  |  squared  |  cubed");
+        System.out.println("------  |  -------  |  -----");
+        for (int i = 1; i <= n; i++){
+            int number = i;
+            int squared = i*i;
+            int cubed = i*i*i;
+            if (number <= 9){
+                System.out.printf("%d       |", number);
+            }
+            if (number > 9 && number < 100){
+                System.out.printf("%d      |", number);
+            }
+            if (squared <= 9){
+                System.out.printf("  %d        |", squared);
+            }
+            if (squared > 9 && number < 100){
+                System.out.printf("  %d       |", squared);
+            }
+            if (cubed <= 9) {
+                System.out.printf("  %d    %n", cubed);
+            }
+            if (cubed > 9 && number < 100){
+                System.out.printf("  %d   %n", cubed);
+            }
+        }
     }
 
 }
