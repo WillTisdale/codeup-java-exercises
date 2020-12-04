6aa8c7756708b2eb9c88c3d915cce171854a405a
William Tisdale <will.tisdale.31@gmail.com>
2020-12-04 10:54:51 -0600

    completed strings part 1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -65,80 +65,87 @@ public class ControlFlowExercises {
 //            if (number > 9 && number < 100){
 //                System.out.printf("%d      |", number);
 //            }
+//            if (number > 99 && number < 1000){
+//                System.out.printf("%d     |", number);
+//            }
 //            if (squared <= 9){
 //                System.out.printf("  %d        |", squared);
 //            }
-//            if (squared > 9 && number < 100){
+//            if (squared > 9 && squared < 100){
 //                System.out.printf("  %d       |", squared);
 //            }
-//            if (cubed <= 9) {
-//                System.out.printf("  %d    %n", cubed);
+//            if (squared > 99 && squared < 1000){
+//                System.out.printf("  %d      |", squared);
 //            }
-//            if (cubed > 9 && number < 100){
-//                System.out.printf("  %d   %n", cubed);
+//            if (squared > 999 && squared < 10000){
+//                System.out.printf("  %d     |", squared);
 //            }
+//            if (squared > 9999 && squared < 100000){
+//                System.out.printf("  %d    |", squared);
+//            }
+//            System.out.printf("  %d    %n", cubed);
 //        }
 
-        System.out.printf("Enter a numerical grade between 0 and 100: ");
-        int grade = sc.nextInt();
-        String more = "Yes";
-        int count = 0;
-            do {
-                ++count;
-                if (grade > 100 || grade < 0){
-                    --count;
-                    System.out.println("You must enter a number between 0 and 100.");
-                }
-                if (grade <= 100 && grade >= 88) {
-                    if (grade >= 99) {
-                        System.out.printf("%d.) %d is an A+%n", count, grade);
-                    } else if (grade <= 89) {
-                        System.out.printf("%d.) %d is an A-%n", count, grade);
-                    } else {
-                        System.out.printf("%d.) %d is an A%n", count, grade);
-                    }
-                }
-                if (grade <= 87 && grade >= 80) {
-                    if (grade >= 86) {
-                        System.out.printf("%d.) %d is a B+%n", count, grade);
-                    } else if (grade <= 81) {
-                        System.out.printf("%d.) %d is a B-%n", count, grade);
-                    } else {
-                        System.out.printf("%d.) %d is a B%n", count, grade);
-                    }
-                }
-                if (grade <= 79 && grade >= 67) {
-                    if (grade >= 78) {
-                        System.out.printf("%d.) %d is a C+%n", count, grade);
-                    } else if (grade <= 68) {
-                        System.out.printf("%d.) %d is a C-%n", count, grade);
-                    } else {
-                        System.out.printf("%d.) %d is a C%n", count, grade);
-                    }
-                }
-                if (grade <= 66 && grade >= 60) {
-                    if (grade >= 65) {
-                        System.out.printf("%d.) %d is a D+%n", count, grade);
-                    } else if (grade <= 61) {
-                        System.out.printf("%d.) %d is a D-%n", count, grade);
-                    } else {
-                        System.out.printf("%d.) %d is a D%n", count, grade);
-                    }
-                }
-                if (grade <= 59 && grade >= 0) {
-                    System.out.printf("%d.) %d is an F%n", count, grade);
-                }
-                try
-                {
-                    System.out.printf("Enter another grade: ");
-                    grade = sc.nextInt();
-                }
-                catch (InputMismatchException exception)
-                {
-                    System.out.println("Thank you!");
-                    more = "no";
-                }
-            }while(more.equalsIgnoreCase("yes"));
+//        System.out.printf("Enter a numerical grade between 0 and 100: ");
+//        int grade = sc.nextInt();
+//        String more = "Yes";
+//        int count = 0;
+//            do {
+//                ++count;
+//                if (grade > 100 || grade < 0){
+//                    --count;
+//                    System.out.println("You must enter a number between 0 and 100.");
+//                }
+//                if (grade <= 100 && grade >= 88) {
+//                    if (grade >= 99) {
+//                        System.out.printf("%d.) %d is an A+%n", count, grade);
+//                    } else if (grade <= 89) {
+//                        System.out.printf("%d.) %d is an A-%n", count, grade);
+//                    } else {
+//                        System.out.printf("%d.) %d is an A%n", count, grade);
+//                    }
+//                }
+//                if (grade <= 87 && grade >= 80) {
+//                    if (grade >= 86) {
+//                        System.out.printf("%d.) %d is a B+%n", count, grade);
+//                    } else if (grade <= 81) {
+//                        System.out.printf("%d.) %d is a B-%n", count, grade);
+//                    } else {
+//                        System.out.printf("%d.) %d is a B%n", count, grade);
+//                    }
+//                }
+//                if (grade <= 79 && grade >= 67) {
+//                    if (grade >= 78) {
+//                        System.out.printf("%d.) %d is a C+%n", count, grade);
+//                    } else if (grade <= 68) {
+//                        System.out.printf("%d.) %d is a C-%n", count, grade);
+//                    } else {
+//                        System.out.printf("%d.) %d is a C%n", count, grade);
+//                    }
+//                }
+//                if (grade <= 66 && grade >= 60) {
+//                    if (grade >= 65) {
+//                        System.out.printf("%d.) %d is a D+%n", count, grade);
+//                    } else if (grade <= 61) {
+//                        System.out.printf("%d.) %d is a D-%n", count, grade);
+//                    } else {
+//                        System.out.printf("%d.) %d is a D%n", count, grade);
+//                    }
+//                }
+//                if (grade <= 59 && grade >= 0) {
+//                    System.out.printf("%d.) %d is an F%n", count, grade);
+//                }
+//                try
+//                {
+//                    System.out.printf("Enter another grade: ");
+//                    grade = sc.nextInt();
+//                }
+//                catch (InputMismatchException exception)
+//                {
+//                    System.out.println("Thank you!");
+//                    more = "no";
+//                }
+//            }while(more.equalsIgnoreCase("yes"));
 
 
 
---
+++ src/StringExercises.java
@@ -0,0 +1,18 @@
+public class StringExercises {
+
+    public static void main(String[] args){
+
+        String pinkFloyd = "We don't need no education%nWe don't need no thought control";
+        System.out.printf(pinkFloyd);
+
+        String weird = "%nCheck \"this\" out!, \"s inside of \"s!";
+        System.out.printf(weird);
+
+        String windows = "%nIn windows, the main drive is usually C:\\";
+        System.out.printf(windows);
+
+        String slash = "%nI can do backslashes \\, double backslashes \\\\,%nand the amazing triple backslash\\\\\\!";
+        System.out.printf(slash);
+    }
+
+}
